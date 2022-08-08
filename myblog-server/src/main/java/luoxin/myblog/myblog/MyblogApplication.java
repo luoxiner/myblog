f64c73cc7a0ce6fe987531700a5e6d1c0103de7a
luoxin <luoxin@outlook.com>
2022-08-08 21:52:22 +0800

    init

---
+++ myblog-server/src/main/java/luoxin/myblog/myblog/MyblogApplication.java
@@ -0,0 +1,15 @@
+package luoxin.myblog.myblog;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luoxi
+ */
+@SpringBootApplication
+public class MyblogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogApplication.class);
+    }
+}
